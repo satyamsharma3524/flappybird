8aeac20b87b0dc726d5941b11e9896fa67f2e483
satyamsharma3524 <satyamsharma3524@gmail.com>
2021-06-22 09:33:03 +0530

    Some Improvements on homepage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random # For generating random numbers
 import sys # We will use sys.exit to exit the program
 import pygame
-from pygame.locals import * # Basic pygame imports
+from pygame.locals import * # Basic pygame import
 
 # Global Variables for the game
 FPS = 32
